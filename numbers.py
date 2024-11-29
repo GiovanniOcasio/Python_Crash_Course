8c7ceee2db564de5071dd62a40337abfac62898a
Giovanni Ocasio <95875715+GiovanniOcasio@users.noreply.github.com>
2024-11-28 19:02:20 -0500

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,47 @@
+#---------------------------------------------------------------------------Integers--------------------------------------------------------------------------
+#Integers are whole numbers in Python. Users can conducted mathematical equations such as addition(+), subtraction(-), division(/), and multiplication(*)
+print("10 - 4 =")
+print(10 - 4)
+
+print("12 + 9 =")
+print(12 + 9)
+
+print("81 / 9 =")
+print(81 / 9)
+
+print("10 * 10 =")
+print(10 * 10)
+
+#---------------------------------------------------------------------------Exponents-------------------------------------------------------------------------
+#Using double asterisk (**) individuals can conduct exponential equations
+print("3 ** 3")
+print(3 ** 3)
+print("3 * 3 * 3")
+print(3 * 3 * 3)
+
+#-----------------------------------------------------------------------------Floats--------------------------------------------------------------------------
+#Floats refer to numbers that contain a decimal (.). As you may have noticed earlier, whenever you divide integers or floats it will always result in a float.
+print("81 / 9")
+print(81 / 9)
+
+print("97 / 4")
+print(97 / 4)
+
+print("10 - 4 =")
+print(10.8 - 4.6)
+print("Notice that Python prints a an arbitrary number. This is common in all languages just ignore the excessive 0s")
+
+print("12 + 9 =")
+print(12.9 + 9.1)
+
+print("10.13 * 10.45 =")
+print(10.13 * 10.45) 
+
+print("9.1 * 9 =")
+print(9.1 * 9) 
+print("This may be common sense but combining a float and an integer will result in an float")
+
+#---------------------------------------------------------------------------Large Numbers---------------------------------------------------------------------
+#Large numbers can be stored using an underscore
+universe_age = 14_000_000_000
+print(universe_age)
